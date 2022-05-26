421b6dfa8faa05d27774f6640e25b5d90700c458
AAA_shuibiao <aaashuibiao@gmail.com>
2022-05-26 12:16:00 -0400

    EDIT and Packagefied Download Module
    * Prepared __init__.py
    * Beautified download.py
        - (CHANGED naming)
    * ADD docstrings

---
+++ downloader/__init__.py
@@ -0,0 +1,3 @@
+"""A minimum download module for getting songs from outside sources."""
+
+from .download import getsong_from_url
---
+++ downloader/download.py
@@ -0,0 +1,67 @@
+"""
+    Download songs from the internet.
+
+    Functions:
+        getsong_from_url(url, outfile_name, logger) --> None:
+"""
+
+from __future__ import unicode_literals
+import youtube_dl
+
+
+def getsong_from_url(url, outfile_name='stWavFile', logger=None):
+    """
+    Use specified URL to download song from the internet and save as file.
+    Accept logging for youtube_dl to handle errors.
+    Debugging or warning functionalities pending further implementation.
+
+    Args:
+        url --> str:    The desired song's source URL   | *
+        outfile_name:   File path for saving            | 'stWavFile'
+        logger:         User supplied custom logger     | Default Built-in Logger
+    """
+
+    #Use Monaural only for testing
+    DEBUG_FORCE_MONO = False
+
+    class BuiltInLogger:
+        """Custom logger class for future use."""
+
+        def debug(self, msg): """Pending implementation."""
+
+        def warning(self, msg): """Pending implementation."""
+
+        @staticmethod
+        def error(msg):
+            """Print error message on stdout for debugging."""
+            print(msg)
+
+    def my_hook(dic):
+        if dic['status'] == 'finished':
+            print(dic)
+            print("Done downloading, now converting ...")
+
+    ydl_args = { # Properties for the output file
+        'outtmpl': outfile_name + '.%(ext)s',
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '192',
+        }],
+        'logger': BuiltInLogger() if logger is None else logger,
+        'progress_hooks': [my_hook],
+    }
+
+    if DEBUG_FORCE_MONO:
+        ydl_args["postprocessor_args"] = {
+            'ac', '1' # Mono audio
+        }
+
+    # Access youtube_dl and download the wav file
+    with youtube_dl.YoutubeDL(ydl_args) as ydl:
+        try:
+            ydl.cache.remove()
+            ydl.download([url])
+        except youtube_dl.DownloadError as dl_error:
+            print(dl_error)
