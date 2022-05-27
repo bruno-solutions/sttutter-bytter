760dc8828c3c60914881a5378775891ba9de873b
walirexro23 <75510240+walirexro23@users.noreply.github.com>
2022-05-27 09:55:25 -0400

    Added some extra error handling messages

--- downloader/download.py
+++ downloader/download.py
@@ -20,53 +20,56 @@ def getsong_from_url(url, outfile_name='stWavFile', logger=None):
         outfile_name:   File path for saving            | 'stWavFile'
         logger:         User supplied custom logger     | Default Built-in Logger
     """
+    if not youtube_dl:
+        raise ModuleNotFoundError("Youtube-dl not found in you environment. "
+                                  "You need to install youtube-dl to your environment")
+    else:
+        # Use Monaural only for testing
+        DEBUG_FORCE_MONO = False
 
-    # Use Monaural only for testing
-    DEBUG_FORCE_MONO = False
+        class BuiltInLogger:
+            """Custom logger class for future use."""
 
-    class BuiltInLogger:
-        """Custom logger class for future use."""
+            """Pending implementation for debug and warning."""
+            @staticmethod
+            def debug(msg):
+                print(msg)
 
-        """Pending implementation for debug and warning."""
-        @staticmethod
-        def debug(msg):
-            print(msg)
+            @staticmethod
+            def warning(msg):
+                print(msg)
 
-        @staticmethod
-        def warning(msg):
-            print(msg)
+            @staticmethod
+            def error(msg):
+                """Print error message on stdout for debugging."""
+                print(msg)
 
-        @staticmethod
-        def error(msg):
-            """Print error message on stdout for debugging."""
-            print(msg)
+        def my_hook(dic):
+            if dic['status'] == 'finished':
+                print(dic)
+                print("Done downloading, now converting ...")
 
-    def my_hook(dic):
-        if dic['status'] == 'finished':
-            print(dic)
-            print("Done downloading, now converting ...")
-
-    ydl_args = {  # Properties for the output file
-        'outtmpl': outfile_name + '.%(ext)s',
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-        }],
-        'logger': BuiltInLogger() if logger is None else logger,
-        'progress_hooks': [my_hook],
-    }
-
-    if DEBUG_FORCE_MONO:
-        ydl_args["postprocessor_args"] = {
-            'ac', '1'  # Mono audio
+        ydl_args = {  # Properties for the output file
+            'outtmpl': outfile_name + '.%(ext)s',
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'wav',
+                'preferredquality': '192',
+            }],
+            'logger': BuiltInLogger() if logger is None else logger,
+            'progress_hooks': [my_hook],
         }
 
-    # Access youtube_dl and download the wav file
-    with youtube_dl.YoutubeDL(ydl_args) as ydl:
-        try:
-            ydl.cache.remove()
-            ydl.download([url])
-        except youtube_dl.DownloadError as dl_error:
-            print(dl_error)
+        if DEBUG_FORCE_MONO:
+            ydl_args["postprocessor_args"] = {
+                'ac', '1'  # Mono audio
+            }
+
+        # Access youtube_dl and download the wav file
+        with youtube_dl.YoutubeDL(ydl_args) as ydl:
+            try:
+                ydl.cache.remove()
+                ydl.download([url])
+            except (youtube_dl.DownloadError, youtube_dl.utils.ExtractorError) as dl_error:
+                print(dl_error)
