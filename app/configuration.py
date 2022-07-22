9bc3486204aa723fe0f608af19848b06cb34a214
autopulous <john.hart@yahoo.com>
2022-07-22 18:47:20 -0400

    Changing the application name (again...)

--- app/configuration.py
+++ app/configuration.py
@@ -1,4 +1,4 @@
-APPLICATION_NAME: str = "sound_bytter"
+APPLICATION_NAME: str = "sttutter-sound-bytter"
 
 DEFAULT_CHANNELS: int = 2
 DEFAULT_SAMPLE_WIDTH: int = 2  # CD Quality
--- app/setup.py
+++ app/setup.py
@@ -4,7 +4,7 @@ setup(
     name='sttutter-sound-bytter',
     version='0.0.1',
     packages=['loader', 'logger', 'slicer', 'tagger', 'tester', 'audioprocessor'],
-    package_dir={'': 'app/loader'},
+    package_dir={'': 'app'},
     url='www.sttutter.com',
     license='MIT',
     author='Sttutter',
